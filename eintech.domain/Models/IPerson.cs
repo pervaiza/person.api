a2dfaf4aae3d5c5772aa9ce89753c86e7ec0a9b1
pervaiz akhtar <pervaiz.akhtar@cbre.com>
2021-10-17 21:05:15 +0100

    init

---
+++ eintech.domain/Models/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eintech.domain.Models
+{
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string Email { get; set; }
+
+        DateTime CreatedOn { get; set; }
+    }
+}
---
+++ eintech.domain/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eintech.domain.Models
+{
+    public class Person : IPerson
+    {
+        public Person()
+        {
+            CreatedOn = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
